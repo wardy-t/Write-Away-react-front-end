88aec6b499f6010a9206f3720bdab0ff2d31de14
Tom Ward <wardusmcbay@gmail.com>
2025-01-10 15:58:26 +0000

    adding css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -28,18 +28,7 @@ const NavBar = ({ handleSignout }) => {
             </li>
           </ul>
         </nav>
-      ) : (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-            <li>
-              <Link to="/signup">Sign Up</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
+      ) : null }
     </>
   );
 };
